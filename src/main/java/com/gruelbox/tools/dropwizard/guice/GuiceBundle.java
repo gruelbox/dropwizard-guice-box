09c6391aa15e9a8f448e618da514bab9a7136e93
Graham Crockford <badgerwithagun@users.noreply.github.com>
2018-12-03 11:02:50 +0000

    Fix unexpected end tag in Javadoc

--- src/main/java/com/gruelbox/tools/dropwizard/guice/GuiceBundle.java
+++ src/main/java/com/gruelbox/tools/dropwizard/guice/GuiceBundle.java
@@ -9,9 +9,9 @@ package com.gruelbox.tools.dropwizard.guice;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -90,7 +90,6 @@ import io.dropwizard.setup.Environment;
  * the {@link Application#run(io.dropwizard.Configuration, Environment)} phase
  * directly from anywhere in your code when you need to do anything else on
  * startup.
- * </p>
  * </ul>
  *
  * @author Graham Crockford
